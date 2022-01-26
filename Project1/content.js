133543c433c33b48a573407e18c1d526d69c7cac
fatimahaidarr <90494698+fatimahaidarr@users.noreply.github.com>
2022-01-26 10:53:04 -0700

    Add files via upload

---
+++ Project1/content.js
@@ -0,0 +1,12 @@
+console.log("Chrome extension go");
+
+let paragraphs = document.getElementsByTagName('p');
+for(elt of paragraphs){
+    elt.style['background-color'] = '#FF00FF';
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse){
+    console.log(message.txt)
+}
